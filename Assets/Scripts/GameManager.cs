a40bbf5d3de8304b62400f3b58b0f06d8e91ef97
Zachary Hayes <zacharyhayes@MumoZH.local>
2019-08-01 12:43:10 -0500

    Adding core plant class, game manager, UI elements

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿// Zachary Hayes - zachary.j.hayes@gmail.com - 08/01/2019
+// v.01
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WarOfCurrents
+{
+    public class GameManager : MonoBehaviour
+    {
+        float _currentBalance;
+        public Text currentBalanceText;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _currentBalance = 2;
+            currentBalanceText.text = _currentBalance.ToString("c2");
+
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+
+        }
+
+        // Add amount to current balance.
+        public void AddToBalance(float amount)
+        {
+            _currentBalance += amount;
+            currentBalanceText.text = _currentBalance.ToString("c2");
+        }
+
+        // Checks if amount can be afforded by current balance.
+        public bool CanBuy(float amount)
+        {
+            if(amount > _currentBalance)
+                return false;
+            else
+                return true;
+        }
+    }
+}
+
--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -3,44 +3,112 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Plant : MonoBehaviour
+namespace WarOfCurrents
 {
-    int PlantCount;
-    public Text PlantCountText;
-    float BasePlantCost;
-
-    //Game
-    float CurrentBalance;
-    public Text CurrentBalanceText;
-
-    // Start is called before the first frame update.
-    void Start()
+    public class Plant : MonoBehaviour
     {
-        PlantCount = 0;
-        CurrentBalance = 2;
-        BasePlantCost = 1.50f;
-        CurrentBalanceText.text = CurrentBalance.ToString("c2");
-    }
+        public GameManager gameManager;
+        public Slider progressSlider;
+        public Button buyButton;
+        public Text buyButtonText;
+        public Text plantCountText;
 
-    // Update is called once per frame.
-    void Update()
-    {
-        
-    }
+        [SerializeField]
+        float _basePlantCost, _basePlantProfit, _plantTimer,
+            _storeMultiplier = 1.0f;
+        [SerializeField]
+        int _plantCount;
+        [SerializeField]
+        bool _managerUnlocked;
 
-    public void BuyPlantOnClick()
-    {
-        if (BasePlantCost > CurrentBalance)
+        float _currentTimer;
+        bool _startTimer;
+        float _nextPlantCost;
+
+
+        // Start is called before the first frame update.
+        void Start()
         {
-            return; // Can't afford it.
+            plantCountText.text = _plantCount.ToString();
+            _nextPlantCost = _basePlantCost;
+            UpdateBuyButton();
         }
 
-        PlantCount += 1;                                // Update PlantCount.
-        PlantCountText.text = PlantCount.ToString();
-        Debug.Log(PlantCount);
+        // Update is called once per frame.
+        void Update()
+        {
+            UpdateTimer();
+            progressSlider.value = _currentTimer / _plantTimer;                 // Update slider.
+            SetBuyButton();
+        }
 
-        CurrentBalance -= BasePlantCost;                // Update Balance.
-        CurrentBalanceText.text = CurrentBalance.ToString("c2");
-        Debug.Log(CurrentBalance);
+        public void BuyPlantOnClick()
+        {
+            if (gameManager.CanBuy(_nextPlantCost))
+            {
+                _plantCount += 1;                                               // Update PlantCount.
+                plantCountText.text = _plantCount.ToString();
+
+                gameManager.AddToBalance(-_nextPlantCost);                      // Subtract cost.
+
+                _nextPlantCost = (_basePlantCost *
+                    Mathf.Pow(_storeMultiplier,_plantCount));                   // Update next store cost.
+
+                UpdateBuyButton();
+            } // else can't afford it.
+        }
+
+        public void PowerOnClick()
+        {
+            if (!_startTimer && _plantCount > 0)
+            {
+                _startTimer = true;
+                ToggleBuyButton(false);
+            }
+        }
+
+        // Update text on buy button to reflect cost of next plant.
+        private void UpdateBuyButton()
+        {
+            buyButtonText.text = "Buy " + _nextPlantCost.ToString("c2");
+        }
+
+        // Show or hide buy button depending on bool.
+        private void ToggleBuyButton(bool showButton)
+        {
+            buyButton.interactable = showButton;
+        }
+
+        private void UpdateTimer()
+        {
+            if (_startTimer)
+            {
+                _currentTimer += Time.deltaTime;
+
+                if (_currentTimer > _plantTimer)
+                {
+                    if (!_managerUnlocked)
+                    {
+                        _startTimer = false;
+                    }
+                    _currentTimer = 0f;
+                    gameManager.AddToBalance(_basePlantProfit * _plantCount);
+                }
+            }
+        }
+
+        // Enables or disables buy button based on current game state.
+        private void SetBuyButton()
+        {
+            if (gameManager.CanBuy(_nextPlantCost) && _currentTimer <= 0f)
+            {
+                ToggleBuyButton(true);
+            }
+            else
+            {
+                ToggleBuyButton(false);
+            }
+        }
     }
+
 }
